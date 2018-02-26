49ff46e3194e1203ad46810960617903b28a41e5
Sukhyun Kwon <shkwon23@nate.com>
2018-02-26 22:14:23 +0900

    GetMode help add

---
+++ man/GetMode.R
@@ -0,0 +1,17 @@
+GetMode <- function(x, order=1, type='value'){
+  # mode count
+  count_tab <- sort(table(x), decreasing = T)
+  mode_count <- count_tab[order]
+  # mode value
+  value_tab <- names(count_tab)
+  mode_value <- value_tab[order]
+  if(type=='value') {
+    result <- mode_value
+  } else if(type=='count'){
+    result <- mode_count
+  }
+  names(result) <- NULL
+  return(result)
+}
+
+
