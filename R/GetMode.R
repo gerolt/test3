c80f8b141460550059afd75cd3992f98c3df278e
Sukhyun Kwon <shkwon23@nate.com>
2018-02-26 22:01:17 +0900

    add help2

--- R/GetMode.R
+++ R/GetMode.R
@@ -1,3 +1,16 @@
+#' Mode, orderly
+#'
+#' The most frequent value of a random variable. You can see the number or value of mode.
+#'
+#' @param x object to be ordered.
+#' @param order Order of modes.
+#' @param type Type of returned value. Count, Value.
+#' @return The most frequent value or number of a random variable.
+#' @examples
+#' GetMode(iris$Species, order=2, type='count') # The number of Second mode
+#' GetMode(iris$Species, order=1, type='value') # The Value of first mode
+#' @export
+#' @importFrom dplyr
 GetMode <- function(x, order=1, type='value'){
   # mode count
   count_tab <- sort(table(x), decreasing = T)
