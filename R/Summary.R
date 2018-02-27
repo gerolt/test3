66b80f1adf8097ba96b889af19ba0ad9b1734f6a
Sukhyun Kwon <shkwon23@nate.com>
2018-02-27 19:30:19 +0900

    Update Summary.R

--- R/Summary.R
+++ R/Summary.R
@@ -1,49 +1,216 @@
-Summary <- function(data){
-  # Warning Message
-  reserved_words <- c('if', 'else', 'while', 'function', 'for', 'in', 'next',
-                      'break',' TRUE', 'FALSE','NULL', 'Inf', 'NaN', 'NA')
-  if(sum(names(data) %in% reserved_words) != 0){
-    stop('변수명은 예약어로 사용할 수 없습니다.\n 예약어 : if, else, while, function, for, in, next,
-         break, TRUE, FALSE, NULL, Inf, NaN, NA')
+dashboard <- function(){
+  # Importing packages
+  if(!require(shiny)){install.packages("shiny");library(shiny)}
+  if(!require(shinydashboard)){install.packages("shinydashboard");library(shinydashboard)}
+  if(!require(dplyr)){install.packages("dplyr");library(dplyr)}
+  if(!require(plotly)){install.packages("plotly");library(plotly)}
+  if(!require(ggplot2)){install.packages("ggplot2");library(ggplot2)}
+  if(!require(gmodels)){install.packages("gmodels");library(gmodels)}
+  if(!require(ggmosaic)){install.packages("ggmosaic");library(ggmosaic)}
+
+  # names of data frame
+  dfnamesft <- function(){
+    dfnames <- ls(envir=.GlobalEnv) %>% sapply(get) %>% sapply(is.data.frame)
+    dfnames <- names(dfnames[dfnames==T])
+    return(dfnames)
   }
-  Card <- function(x) length(unique(x))
-  NofNA <- function(x) sum(is.na(x))
-  # Split
-  con <- data[sapply(data, is.number)]
-  if(length(con)==0) con$idx_number <- 1:nrow(con)
-  cat <- data[!sapply(data, is.number)]
-  if(length(cat)==0) cat$idx_number <- as.character(1:nrow(cat))
-  # Continuous
-  con_n <- sapply(con, length)
-  con_na <- sapply(con, NofNA)
-  con_card <-sapply(con, Card)
-  con_dat <- data.frame(cbind(
-    names(con), con_n,  con_na,
-    con_na/con_n, con_card, con_card/con_n,
-    sapply(con, min, na.rm=T), sapply(con, max, na.rm=T),
-    sapply(con, median, na.rm=T), sapply(con, mean, na.rm=T),
-    sapply(con, quantile, 0.25, na.rm=T), sapply(con, quantile, 0.75, na.rm=T),
-    sapply(con, sd, na.rm=T)
-  ))
-  # Categorical
-  cat_n <- sapply(cat, length)
-  cat_na <- sapply(cat, NofNA)
-  cat_card <-sapply(cat, Card)
-  catm1c <- sapply(cat, GetMode, order=1, type='count')
-  catm2c <- sapply(cat, GetMode, order=2, type='count')
-  cat_dat <- data.frame(cbind(
-    names(cat),
-    cat_n, cat_na, cat_na/cat_n, cat_card, cat_card/cat_n,
-    sapply(cat, GetMode, order=1, type='value'),
-    catm1c, catm1c/cat_n,
-    sapply(cat, GetMode, order=2, type='value'),
-    catm2c, catm2c/cat_n
-  ))
-  names(con_dat) <-
-    c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
-      'Minimum', 'Q1', 'Mean', 'Median', 'Q3', 'Maximum', 'Std')
-  names(cat_dat) <-
-    c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
-      'Mode1', 'Mode1.C', 'Mode1.R', 'Mode2', 'Mode2.C', 'Mode2.R')
-  return(list(Categorical = cat_dat, Continuous = con_dat))
+  dfnames <- dfnamesft()
+
+  header <- dashboardHeader(
+    title = 'Data Summary'
+  )
+  sidebar <- dashboardSidebar(
+    sidebarMenu(
+      menuItem('Data', tabName = 'menu_data'),
+      menuItem('Plot', tabName = 'menu_plot'),
+      absolutePanel(
+        bottom = 20, right = 20, width = 300,
+        draggable = TRUE,
+        style = "opacity: 0.70;color: #FF0000",
+        wellPanel(
+          selectInput('sel_posi', 'Position(hist ONLY)', c('stack', 'identity', 'dodge')),
+          htmlOutput('selectUIs'),
+          sliderInput('sli_binw', 'Number of bin width(hist ONLY)', min = 0.1, max=2, step = 0.1, value = 0.2),
+          htmlOutput('selectUIw')
+        )
+      )
+    )
+  )
+  body <- dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = 'menu_data',
+        fluidPage(
+          column(
+            width = 2,
+            selectInput('sel_data','Data',dfnames),
+            box(
+              title = 'Row, Colum of Plots', solidHeader = T, collapsible = T,width = 13,
+              numericInput('num_plotrow', 'Number of row', value = 7),
+              numericInput('num_plotcol', 'Number of col', value = 8)
+            ),
+            box(
+              title = 'Plot Parameters', solidHeader = T, collapsible = T,width = 13,
+              selectInput('sel_ptype', 'Plot type', c('point', 'boxplot', 'histogram', 'mosaic')),
+              htmlOutput('selectUIx'),
+              htmlOutput('selectUIy'),
+              htmlOutput('selectUIc'),
+              htmlOutput('selectUIf'),
+              htmlOutput('selectUIfacet'),
+              radioButtons('radio_axis','Facet axis',c('col','row'),inline = T),
+              sliderInput('sli_alpha', 'alpha', min = 0, max=1, value = 1),
+              submitButton("Apply Changes")
+            )
+          ),
+          tabBox(
+            width = 10,
+            tabPanel(
+              title = 'Data',
+              fluidPage(
+                dataTableOutput('dt_raw')
+              )
+            ),
+            tabPanel(
+              title = 'Summary',
+              fluidPage(
+                fluidRow(
+                  dataTableOutput('sum_cat')
+                ),
+                fluidRow(
+                  dataTableOutput('sum_con')
+                )
+              )
+            ),
+            tabPanel(
+              title = 'Plots',
+              fluidPage(
+                plotOutput('plot1',height="1000px")
+              )
+            ),
+            tabPanel(
+              title = 'Detailed Plot',
+              fluidPage(
+                plotlyOutput('detailplot',height="800px")
+              )
+            ),
+            tabPanel(
+              title = 'Cross Table',
+              fluidPage(
+                verbatimTextOutput('crosstab')
+              )
+            )
+          )
+        )
+      ),
+      tabItem(
+        tabName = 'menu_plot',
+        fluidRow(
+          tabBox(
+            width = 7,
+            tabPanel('one', 'tab_one1'),
+            tabPanel('two', 'tab_two1')
+          )
+        )
+      )
+    )
+  )
+  ui <- dashboardPage(header = header, sidebar = sidebar, body = body)
+  server <- function(input, output){
+    data.r <- reactive({
+      return(get(input$sel_data))
+    })
+    summary.r <- reactive({
+      return(Summary(data.r()))
+    })
+    output$dt_raw <- renderDataTable(data.r(),
+                                     options = list(
+                                       pageLength = 12,
+                                       scrollX = T
+                                     )
+    )
+    output$sum_cat <- renderDataTable(
+      summary.r()[['Categorical']],
+      options = list(
+        pageLength = 12,
+        scrollX = T
+      )
+    )
+    output$sum_con <- renderDataTable(
+      summary.r()[['Continuous']],
+      options = list(
+        pageLength = 12,
+        scrollX = T
+      )
+    )
+    output$plot1 <- renderPlot({
+      idx_cat <- summary.r()$Categorical[summary.r()$Categorical$Cardinality<11,] %>% rownames()
+      idx_con <- summary.r()$Continuous[summary.r()$Continuous$n>summary.r()$Continuous$Cardinality,] %>% rownames()
+      par(mfrow=c(input$num_plotrow,input$num_plotcol))
+      for(i in idx_cat){
+        barplot(table(data.r()[[i]]), main = i)
+      }
+      for(j in idx_con){
+        hist(data.r()[[j]], main = j, xlab = '')
+      }
+    })
+    varnames.r <- reactive({
+      return(names(data.r()))
+    })
+    varnames_num.r <- reactive({
+      return(varnames.r()[sapply(data.r(), is.number)])
+    })
+    varnames_cha.r <- reactive({
+      return(varnames.r()[!sapply(data.r(), is.number)])
+    })
+    facet.r <- reactive({
+      if(input$sel_facet=='NULL') result <- NULL else if(input$radio_axis=='col'){
+        result <- facet_grid(paste('~', input$sel_facet, sep = ''))
+      } else if(input$radio_axis=='row'){
+        result <- facet_grid(paste(input$sel_facet,'~.', sep = ''))
+      }
+      return(result)
+    })
+    output$selectUIx <- renderUI({
+      selectInput('sel_x', 'X variable', c(1,varnames.r()), varnames.r()[1])
+    })
+    output$selectUIy <- renderUI({
+      selectInput('sel_y', 'Y variable', c(1,varnames.r()), varnames.r()[2])
+    })
+    output$selectUIc <- renderUI({
+      selectInput('sel_c', 'Color', c('NULL',varnames.r()), 'NULL')
+    })
+    output$selectUIf <- renderUI({
+      selectInput('sel_f', 'Fill', c('NULL',varnames.r()), 'NULL')
+    })
+    output$selectUIfacet <- renderUI({
+      selectInput('sel_facet', 'Facet', c('NULL',varnames_cha.r()), 'NULL')
+    })
+    output$selectUIs <- renderUI({
+      selectInput('sel_s', 'Shape(point ONLY)', c('NULL',varnames_cha.r()), 'NULL')
+    })
+    output$selectUIw <- renderUI({
+      selectInput('sel_w', 'Weight(mosaic ONLY)', c('NULL',varnames_cha.r()), 'NULL')
+    })
+    ptype.r <- reactive({
+      switch (input$sel_ptype,
+              'point' = geom_point(aes_string(x=input$sel_x, y=input$sel_y, shape=input$sel_s)
+                                   , alpha=input$sli_alpha),
+              'boxplot' = geom_boxplot(aes_string(x=input$sel_x, y=input$sel_y), alpha=input$sli_alpha),
+              'histogram' = geom_histogram(aes_string(x=input$sel_x),
+                                           position = input$sel_posi, alpha=input$sli_alpha,
+                                           binwidth = input$sli_binw),
+              'mosaic' = geom_mosaic(aes_(weight = input$sel_w, x = product(input$sel_x, input$sel_y)))
+      )
+    })
+    output$detailplot <- renderPlotly({
+      g <- ggplot(data.r()) + ptype.r() + aes_string(color=input$sel_c, fill=input$sel_c) +
+        facet.r()
+      ggplotly(g)
+    })
+    output$crosstab <- renderPrint({
+      a <- CrossTable(data.r()[[input$sel_x]], data.r()[[input$sel_y]], dnn = c(input$sel_x, input$sel_y))
+    })
   }
+  return(shinyApp(ui, server))
+}
+dashboard()
