ae354cddea6ce93d3f3541f1b942f0e9b6e64601
Sukhyun Kwon <shkwon23@nate.com>
2018-02-27 19:41:34 +0900

    Add files via upload

---
+++ R/Summary.R
@@ -0,0 +1,52 @@
+Summary <- function(data){
+  # Warning Message
+  reserved_words <- c('if', 'else', 'while', 'function', 'for', 'in', 'next',
+                      'break',' TRUE', 'FALSE','NULL', 'Inf', 'NaN', 'NA')
+  if(sum(names(data) %in% reserved_words) != 0){
+    stop('변수명은 예약어로 사용할 수 없습니다.\n 예약어 : if, else, while, function, for, in, next,
+         break, TRUE, FALSE, NULL, Inf, NaN, NA')
+  }
+  Card <- function(x) length(unique(x))
+  NofNA <- function(x) sum(is.na(x))
+  # Split
+  con <- data[sapply(data, is.number)]
+  if(length(con)==0) con$idx_number <- 1:nrow(con)
+  cat <- data[!sapply(data, is.number)]
+  if(length(cat)==0) cat$idx_number <- as.character(1:nrow(cat))
+  # Continuous
+  con_n <- sapply(con, length)
+  con_na <- sapply(con, NofNA)
+  con_card <-sapply(con, Card)
+  con_dat <- data.frame(cbind(
+    names(con), con_n,  con_na,
+    con_na/con_n, con_card, con_card/con_n,
+    sapply(con, min, na.rm=T), sapply(con, max, na.rm=T),
+    sapply(con, median, na.rm=T), sapply(con, mean, na.rm=T),
+    sapply(con, quantile, 0.25, na.rm=T), sapply(con, quantile, 0.75, na.rm=T),
+    sapply(con, sd, na.rm=T)
+  ))
+  # Categorical
+  cat_n <- sapply(cat, length)
+  cat_na <- sapply(cat, NofNA)
+  cat_card <-sapply(cat, Card)
+  catm1c <- sapply(cat, GetMode, order=1, type='count')
+  catm2c <- sapply(cat, GetMode, order=2, type='count')
+  cat_dat <- data.frame(cbind(
+    names(cat),
+    cat_n, cat_na, cat_na/cat_n, cat_card, cat_card/cat_n,
+    sapply(cat, GetMode, order=1, type='value'),
+    catm1c, catm1c/cat_n,
+    sapply(cat, GetMode, order=2, type='value'),
+    catm2c, catm2c/cat_n
+  ))
+  names(con_dat) <-
+    c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
+      'Minimum', 'Q1', 'Mean', 'Median', 'Q3', 'Maximum', 'Std')
+  names(cat_dat) <-
+    c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
+      'Mode1', 'Mode1.C', 'Mode1.R', 'Mode2', 'Mode2.C', 'Mode2.R')
+  return(list(Categorical = cat_dat, Continuous = con_dat))
+  }
+
+
+
