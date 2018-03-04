8e232b6484e5956d5d77bd739cb6f490b8c5076b
Sukhyun Kwon <shkwon23@nate.com>
2018-03-04 16:30:58 +0900

    change

--- R/AsDateDataFrame.R
+++ R/AsDateDataFrame.R
@@ -1,8 +1,12 @@
 AsDateDataFrame <- function(data, variable){
   if(!require(dplyr)){install.packages("dplyr");library(dplyr)}
+  vorder <- names(data)
   idx <- which(names(data) == variable)
   data2change <- data[idx]
   data2stay <- data[-idx]
-  invisible(tbl_df(sapply(data2change, as.Date)))
+  dataachange <- tbl_df(sapply(data2change, as.Date))
+  data <- cbind(dataachange, data2stay)
+  data <- data[vorder]
+  invisible(data)
 }
 
