ada3d1749073305fc4194ab2fe73d04e20629461
Sukhyun Kwon <shkwon23@nate.com>
2018-03-07 03:58:33 +0000

    Update some2some.R

--- R/some2some.R
+++ R/some2some.R
@@ -1,6 +1,7 @@
 some2some <- function(data, before, after){
-  for(i in 1:nrow(data)){
+  for(i in names(data)){
     data[[i]] <- ifelse(data[[i]]==before, after, data[[i]])
   }
   invisible(data)
 }
+
