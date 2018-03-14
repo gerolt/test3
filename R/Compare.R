ce3852e23445d6309b6d4407638f21b16816a24b
Sukhyun Kwon <shkwon23@nate.com>
2018-03-14 14:46:04 +0900

    Update Compare.R

--- R/Compare.R
+++ R/Compare.R
@@ -1,11 +1,11 @@
-Compare <- function(data,group,y,order='mean'){
-  if(!require(dplyr)){install.packages("dplyr");library(dplyr)}
-  return(
-    data %>%
-      group_by_at(group) %>%
-      summarise(mean = mean(get(y), na.rm = T),
-                sd = sd(get(y), na.rm = T),
-                length = length(get(y))) %>%
-      arrange_at(order, desc)
-  )
+Compare <- function(data,y,by){
+  if(!require(data.table)){install.packages("data.table");library(data.table)}
+  data <- data.table(data)
+  data[,
+       list(
+         #mean = mean('price'),
+         std = sd(y)
+       ),
+       by = by
+       ]
 }
