8a1841adc29e74acdbff7d60e8ee7dd49d45974f
Sukhyun Kwon <shkwon23@nate.com>
2018-03-21 16:00:20 +0900

    Update Summary.R

--- R/Summary.R
+++ R/Summary.R
@@ -17,33 +17,35 @@ Summary <- function(data){
   con_n <- sapply(con, length)
   con_na <- sapply(con, NofNA)
   con_card <-sapply(con, Card)
-  con_dat <- data.frame(stringsAsFactors = F,
+  con_dat <- data.frame(cbind(
     names(con), con_n,  con_na,
     con_na/con_n, con_card, con_card/con_n,
     sapply(con, min, na.rm=T), sapply(con, max, na.rm=T),
     sapply(con, median, na.rm=T), sapply(con, mean, na.rm=T),
     sapply(con, quantile, 0.25, na.rm=T), sapply(con, quantile, 0.75, na.rm=T),
     sapply(con, sd, na.rm=T)
-  )
+  ))
   # Categorical
   cat_n <- sapply(cat, length)
   cat_na <- sapply(cat, NofNA)
   cat_card <-sapply(cat, Card)
   catm1c <- sapply(cat, GetMode, order=1, type='count')
   catm2c <- sapply(cat, GetMode, order=2, type='count')
-  cat_dat <- data.frame(stringsAsFactors = F,
+  cat_dat <- data.frame(cbind(
     names(cat),
     cat_n, cat_na, cat_na/cat_n, cat_card, cat_card/cat_n,
     sapply(cat, GetMode, order=1, type='value'),
     catm1c, catm1c/cat_n,
     sapply(cat, GetMode, order=2, type='value'),
     catm2c, catm2c/cat_n
-  )
+  ))
   names(con_dat) <-
     c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
       'Minimum', 'Q1', 'Mean', 'Median', 'Q3', 'Maximum', 'Std')
   names(cat_dat) <-
     c('Variable','n', 'Missing', 'Missing.R', 'Cardinality', 'Cardinality.R',
-      'Mode1', 'Mode1.C', 'Mode1.R', 'Mode2', 'Mode2.C', 'Mode2.R')
+      'FirstMode', 'FirstMode.C', 'FirstMode,R', 'SecondMode', 'SecondMode.C',
+      'SecondMode.R')
+  cat_dat$FS.R <- (cat_dat$FirstMode.C + SecondMode.C) / cat_dat$n
   return(list(Categorical = cat_dat, Continuous = con_dat))
 }
