3397d545c86d8f0222f5a7c747a38aaf5bb92cc9
Sukhyun Kwon <shkwon23@nate.com>
2018-04-18 22:20:57 +0900

    Update isnumber.R

--- R/isnumber.R
+++ R/isnumber.R
@@ -1,7 +1,21 @@
+#' Numeric, Integer Vectors
+#'
+#' Creates or coerces objects of type "numeric" or "integer".
+#'
+#' @param x object to be coerced or tested.
+#' @return TRUE / FALSE
+#' @examples
+#' is.number(iris$Sepal.Length)
+#' is.number(iris$Species)
+#' @export
+#' @importFrom dplyr
+
+
 is.number <- function(x){
   crit <- class(x)
   if(length(crit)>=2){
     crit <- paste(crit, collapse = ' ')
   }
-  try(if( crit %in% c('numeric','integer') ) return(TRUE) else return(FALSE),silent = T)
+  try(if( crit %in% c('numeric','integer', 'Date') )
+    return(TRUE) else return(FALSE),silent = T)
 }
